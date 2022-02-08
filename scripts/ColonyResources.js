d679e7f42eb84aef41c3083d909b7464d8576404
Katie Dawkins <katiemdawkins@gmail.com>
2022-02-08 15:11:17 -0600

    algorithm for ColonyResources.js

---
+++ scripts/ColonyResources.js
@@ -0,0 +1,15 @@
+//this module will create a function that produces an output box for colony resources 
+//this function creates a string that prints colony name and colony minerals and tonage
+
+//needs to print colony name + Minerals h2
+//needs to print the string of minerals and tonage - # tons of (mineral)
+
+//import getColonyMinerals - this function needs to be created in database
+
+//define a function that will be exported called ColonyResources -takes an input of GovernorsObj
+    //the html should be an empty string 
+    //+= h2 interpolate (GovernorObj.colonyName) Minerals 
+    //invoke a function - that connects governor with colony- find
+    //invoke a function - that connects colony with colonyMinerals- filtering
+    //output the string- number of tons and (mineral)
+    //return the html
