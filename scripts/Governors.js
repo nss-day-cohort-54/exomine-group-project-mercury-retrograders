62acdcf4b10984a8f9bd6331e2bea625864773ce
Adam Donaghy <donaghyam@gmail.com>
2022-02-08 14:52:40 -0600

    added governors module with algorithm

---
+++ scripts/Governors.js
@@ -0,0 +1,13 @@
+//The purpose of this module is to produce an HTML element of governor selections
+
+//Import governors array from database
+
+//Define variable to store governors array
+
+//Create function to add drop down menu containing governors
+    //Define variable to store empty HTML string
+    //Add drop down menu options to ^ string
+    //Use map.() array method to list governors
+    //Return html string
+
+//Create event listener to invoke setGovernors function
