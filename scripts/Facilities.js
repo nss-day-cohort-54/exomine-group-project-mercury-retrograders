e2eb6dd50da1cef54d468be596ba4a798e7a5ef3
Adam Donaghy <donaghyam@gmail.com>
2022-02-08 15:46:36 -0600

    added Facilities.js

---
+++ scripts/Facilities.js
@@ -0,0 +1,14 @@
+//This module will create a drop down menu with facilities
+
+//Import facilities
+
+//Define variable to store facilities array
+
+//Create function to add drop down menu containing facilities
+    //Define variable to store empty HTML string
+    //Add drop down menu options to ^ string
+    //Use map.() array method to list facilities
+    //Return html string
+
+//Add event listener
+    //Invoke setFacilities function
