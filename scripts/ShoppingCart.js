fe043d69065a480e93fc73d9aa3bfdc6469f2790
Wil Wardrop <wwardrop@gmail.com>
2022-02-08 14:58:35 -0600

    adding shopping cart module and commented out algorithm

---
+++ scripts/ShoppingCart.js
@@ -0,0 +1,10 @@
+//the purpose of this module is to produce an html element with a purchase button
+
+
+
+
+//define a function that takes in an OBJECT from an event listener (purchase button) and displays an interpolated string of mineral purchased
+    //create interpolated string with #tons purchased and material purchased "(#) tons of (material)" and save to variable
+    //creates variable with target div id = "spaceCartOutput"
+    //sets TARGET.innerHTML to the interpolated string
+    //return HTML string
