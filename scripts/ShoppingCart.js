940348069e09aa3d5e65aa5a935a6de0906f0343
Wil Wardrop <wwardrop@gmail.com>
2022-02-14 09:29:41 -0600

    wrote out code for shoppingCart modulem

--- scripts/ShoppingCart.js
+++ scripts/ShoppingCart.js
@@ -1,27 +1,49 @@
 //The purpose of this module is to produce a message to the shopping cart and modify the facilityMinerals and colonyResources once the purchase button is clicked
 
-// import transientState
-// import minerals array
+// import transientState and minerals from database
+import { getTransientState, getMinerals, purchaseMineral } from "./database.js";
+
+const minerals = getMinerals()
 
 // use querySelector to target the html element in mining.js that matches this id: spaceCartOutput
     // save querySelecot to new variable
+const cartOutputTarget = document.querySelector("#spaceCartOutput")
+
 
 //Add eventListener to document listening for "facilityMineralChanged"
     // once facilityMineralChanged is heard
         // invoke shoppingCartDisplay function
-        
+
+document.addEventListener(
+    "facilityMineralChanged",
+    (customEvent) => {
+        shoppingCartDisplay()
+    })
 
 // create a shoppingCartDisplay function that prints the shopping cart display message 
     //representing 1 ton of whichever facilityMineral the user chose
-    
-    // invoke the transientState and save to a variable
 
+const shoppingCartDisplay = () => {
+
+    // invoke the transientState and save to a variable
+const transientObject = getTransientState()
+}
     // use find to iterate through the minerals array
         // return foundMineralObject where mineral.id === transientstate.facilityMineralObject.mineralId
+    const foundMineralObject = minerals.find(mineral => {return mineral.id === transientObject.mineralId})
 
     //set the content target to the innerHTML equal to an interpolated string that reads "1 ton of ${"foundMineralObject.name}
-
+    cartOutputTarget.innerHTML = `1 ton of ${foundMineralObject.name}`
 
 // add eventListener to the purchaseButton
     // if the purcahseButton is clicked
         // invoke the purchaseMineral function in database.js
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        if(clickEvent.target.id === "purchaseButton"){
+            purchaseMineral()
+        }
+    }
+)
