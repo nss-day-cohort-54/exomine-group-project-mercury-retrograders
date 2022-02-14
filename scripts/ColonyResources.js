c99f7b2c8d496d1428bd7d69ca9779315bcc9cd3
Katie Dawkins <katiemdawkins@gmail.com>
2022-02-14 11:07:07 -0600

    code completed for colonyResources.js

--- scripts/ColonyResources.js
+++ scripts/ColonyResources.js
@@ -2,59 +2,71 @@
 // Colony Minerals will change when governor is chosen
 
 //import setGovernors
-//import getMinerals
+//import getMinerals ?? getFacilityMinerals
 //import getTransientState
 //import getColonyMinerals
 //import setColonyMinerals
-
+import { setGovernors } from "./database.js"
+import { getColonyMinerals } from "./database.js"
+import { getTransientState } from "./database.js"
+import { setColonyMinerals} from " ./database.js"
+import { getMinerals } from "./database.js"
 
 
 //create an event listener to listen for  "governorChange" in the document
     // when "governorChange" is heard
         // invoke the ColonoyResources function that returns an html interpolated string- Includes inventory for each colony and the h2 updated colony name
 
-// use querySelector or getElementById to target the "colonyResources" section in mining.js
-//using querySelector to repopulate the colonyResources section with the new html
+
+document.addEventListener(
+    "governorChange",
+    (changeEvent) => {
+        targetHTML.innerHTML = ColonyResources()
+    }
+)
+
+//use querySelector to repopulate the colonyResources section with the new html
+const targetHTML = document.querySelector(".colonyResources")
+
 
 //Create a function - ColonyResources that returns the html for Colony Name and the inventory in an interpolated string
-    //getTransientState
-    //getColonyMinerals
+const ColonyResources = () => {
+    //getTransientState  //getColonyMinerals //getMinerals
+    const transientStateObject = getTransientState()
+    const colonyMineralsObjectsArray = getColonyMinerals()
+    const mineralsArray = getMinerals()
 
-    
-    //declare an html variable that will be an h2 element with colony.name + Minerls
-        //<h2> ${transientStateObject.colonyName}'s Minerals </h2>
+    //declare an html variable that will be an h2 element with colony.name + Minerals //<h2> ${transientStateObject.colonyName}'s Minerals </h2>
+    let html = `<h2>${transientStateObject.colonyName}'s Minerals</h2>`
+
+    // declare  variable foundMineralObjectArray  use the filter method to  colonyMineralsObjectsArray
+    const foundColonyMineralObjectArray = colonyMineralsObjectsArray.filter(colonyMineralObject => {
+        //return an array of all objects with colonyMineralObject.colonyId matches transientStateObject.colonyId
+        return colonyMineralObject.colonyId === transientStateObject.colonyId
+    })
+    // html += <ul> appending the unordered list element to the html string 
+    html += "<ul>"
+
+    //Use forEach method to iterate through foundMineralObjectArray to create a new list of colony resource items
+    foundColonyMineralObjectArray.forEach(foundColonyMineralObject => {
+        //interate through minerals array use find method to return  mineral object with an id that matches colonyMineralObject.mineralId
+        const foundMineralObject = mineralsArray.find(mineralObject =>{
+            return mineralObject.id === foundColonyMineralObject.mineralId
+        })
+        //append the list item element to the html string
+        //    `<li class="colonyResourcesList"> ${ number} tons of ${mineral name} </li>`
+            html += `<li class="colonyResourcesList"> ${foundColonyMineralObject.tonage} tons of ${foundMineralObject.name}</li>`
+    })
+
+    //Close ul HTML element
+        html += "</ul>"
+    //Return the HTML string
+        return html
+
+}
 
 
-
-        // declare  variable foundMineralObjectArray  use the filter method to  colonyMineralsObjectsArray
-/*
-transstate has 
-    governorId
-    governorName
-    colonyId
-    colonyName
-
-colonyMineralObject
-    id
-    colonyId
-    mineralId
-    tonage
-*/
-            //return an array of all objects with colonyMineralObject.colonyId matches transientStateObject.colonyId
-            
-        // html += <ul> appeending the unordered list element to the html string 
-        //Use forEach method to iterate through foundMineralObjectArray to create a new list of colony resource items
-                //interate through minerals array use find method to return  mineral object with an id that matches colonyMineralObject.mineralId
-                
-                //foundMineralObject {
-                    //id
-                    //name
-                //}
-                //append the list item element to the html string
-            //    `<li class="colonyResourcesList"> ${ number} tons of ${mineral name} </li>`
-
-        //Close ul HTML element
-        //Return the HTML string
+        
         
 
 
